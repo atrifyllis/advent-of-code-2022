11b2570ef62868eac9a789a9ba4ebf7ef50f8e71
otinanism <otinanism@gmail.com>
2022-12-24 17:00:18 +0200

    Day 5 part 2 refactoring.

--- src/main/kotlin/gr/alx/adventofcode/day5/CratesArranger.kt
+++ src/main/kotlin/gr/alx/adventofcode/day5/CratesArranger.kt
@@ -5,11 +5,6 @@ import java.util.*
 
 class CratesArranger {
 
-    //    fun readInput(lines: List<String>) {
-//        val crateLines = lines.subList(0, lines.indexOf("")).reversed()
-//        val procedureLines = lines.subList(lines.indexOf("")+ 1 , lines.size + 1)
-//
-//    }
     fun readStacks(lines: List<String>): MutableMap<Int, Deque<Char>> {
         val stacks = mutableMapOf<Int, Deque<Char>>()
         val lineIndexToStackIndex = mutableMapOf<Int, Int>()
@@ -36,23 +31,9 @@ class CratesArranger {
     }
 
     private fun isValid(char: Char) = !char.isWhitespace() && char != '[' && char != ']'
-    fun readProcedureLines(lines: List<String>): List<Procedure> {
-        return lines.map {
-            Procedure(size = extractSize(it), from = extractFrom(it), to = extractTo(it))
-        }
-    }
 
-    private fun extractFrom(line: String): Int {
-        return line.substringAfter("from ").substringBefore(" to").toInt()
-    }
+    fun readProcedureLines(lines: List<String>): List<Procedure> = lines.map { Procedure.from(it) }
 
-    private fun extractTo(line: String): Int {
-        return line.substringAfter("to ").toInt()
-    }
-
-    private fun extractSize(line: String): Int {
-        return line.substringAfter("move ").substringBefore(" from").toInt()
-    }
 
     fun execute(lines: List<String>): String {
 
@@ -61,16 +42,14 @@ class CratesArranger {
 
         val stacks = applyAll(readStacks(crateLines), readProcedureLines(procedureLines))
 
-        return stacks.toSortedMap().values.map {
-            it.peek()
-        }.joinToString("")
+        return stacks.toSortedMap().values
+            .map { it.peek() }
+            .joinToString("")
 
     }
 
     private fun applyAll(stacks: MutableMap<Int, Deque<Char>>, procedures: List<Procedure>): MutableMap<Int, Deque<Char>> {
-        procedures.forEach {
-            applyProcedure(it, stacks)
-        }
+        procedures.forEach { applyProcedure(it, stacks) }
         return stacks
     }
 
@@ -88,28 +67,21 @@ class CratesArranger {
 
         val stacks = applyAllPart2(readStacks(crateLines), readProcedureLines(procedureLines))
 
-        return stacks.toSortedMap().values.map {
-            it.peek()
-        }.joinToString("")
+        return stacks.toSortedMap().values
+            .map { it.peek() }
+            .joinToString("")
     }
 
     private fun applyAllPart2(stacks: MutableMap<Int, Deque<Char>>, procedures: List<Procedure>): MutableMap<Int, Deque<Char>> {
-        procedures.forEach {
-            applyPart2Procedure(it, stacks)
-        }
+        procedures.forEach { applyPart2Procedure(it, stacks) }
         return stacks
     }
 
     private fun applyPart2Procedure(procedure: Procedure, stacks: MutableMap<Int, Deque<Char>>) {
-
-        val temp = mutableListOf<Char>()
-        for (i in 1..procedure.size) {
-            temp.add(stacks[procedure.from]?.pop()!!)
-
-        }
-        temp.reversed().forEach {
-            stacks[procedure.to]?.push(it)
-        }
+        IntRange(1, procedure.size)
+            .map { stacks[procedure.from]?.pop() }
+            .reversed()
+            .forEach { stacks[procedure.to]?.push(it) }
     }
 
 
--- src/main/kotlin/gr/alx/adventofcode/day5/Procedure.kt
+++ src/main/kotlin/gr/alx/adventofcode/day5/Procedure.kt
@@ -1,3 +1,24 @@
 package gr.alx.adventofcode.day5
 
-data class Procedure(val size: Int, val from: Int, val to: Int)
+data class Procedure(val size: Int, val from: Int, val to: Int) {
+
+    companion object {
+        private fun extractSize(line: String): Int {
+            return line.substringAfter("move ").substringBefore(" from").toInt()
+        }
+
+        private fun extractFrom(line: String): Int {
+            return line.substringAfter("from ").substringBefore(" to").toInt()
+        }
+
+        private fun extractTo(line: String): Int {
+            return line.substringAfter("to ").toInt()
+        }
+
+        fun from(line: String): Procedure {
+            return Procedure(size = extractSize(line), from = extractFrom(line), to = extractTo(line))
+        }
+    }
+
+
+}
