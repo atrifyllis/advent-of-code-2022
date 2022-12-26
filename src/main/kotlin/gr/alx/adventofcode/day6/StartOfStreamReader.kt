2fb2d478fdb224aabc0cce78168fecf2a99484b5
otinanism <otinanism@gmail.com>
2022-12-26 18:41:19 +0200

    Day 6 part 2.

--- src/main/kotlin/gr/alx/adventofcode/day6/StartOfStreamReader.kt
+++ src/main/kotlin/gr/alx/adventofcode/day6/StartOfStreamReader.kt
@@ -1,20 +1,20 @@
 package gr.alx.adventofcode.day6
 
 class StartOfStreamReader {
-    fun detectStartOfStream(stream: String): Int {
+    fun detectStartOfStream(stream: String, size: Int): Int {
         var acc = ""
         stream.forEachIndexed { index, value ->
             acc += value
-            if (acc.length >= 4 && noDuplicatesInLast4Chars(acc)) {
+            if (acc.length >= size && noDuplicatesInLast4Chars(acc, size)) {
                 return index + 1
             }
         }
         return -1
     }
 
-    private fun noDuplicatesInLast4Chars(acc: String): Boolean {
-        val sequence = acc.takeLast(4)
-        return sequence.chars().distinct().count() == 4L
+    private fun noDuplicatesInLast4Chars(acc: String, size: Int): Boolean {
+        val sequence = acc.takeLast(size)
+        return sequence.chars().distinct().count() == size.toLong()
     }
 
 }
--- src/test/kotlin/gr/alx/adventofcode/day6/StartOfStreamReaderTest.kt
+++ src/test/kotlin/gr/alx/adventofcode/day6/StartOfStreamReaderTest.kt
@@ -10,7 +10,7 @@ class StartOfStreamReaderTest {
     fun `should detect four consecutive distinct letters`() {
         val input = "mjqjpqmgbljsphdztnvjfqwrcgsmlb"
 
-        val indexOfStartOfStream: Int = StartOfStreamReader().detectStartOfStream(input)
+        val indexOfStartOfStream: Int = StartOfStreamReader().detectStartOfStream(input, 4)
 
         assertThat(indexOfStartOfStream).isEqualTo(7)
     }
@@ -19,7 +19,17 @@ class StartOfStreamReaderTest {
     fun `should read input`() {
         val stream = ReadInputHelper.readTextFromResource("/day6/stream.txt")
 
-        val indexOfStartOfStream: Int = StartOfStreamReader().detectStartOfStream(stream)
+        val indexOfStartOfStream: Int = StartOfStreamReader().detectStartOfStream(stream, 4)
+
+        assertThat(indexOfStartOfStream).isEqualTo(1198)
+
+    }
+
+    @Test
+    fun `should read input for part 2`() {
+        val stream = ReadInputHelper.readTextFromResource("/day6/stream.txt")
+
+        val indexOfStartOfStream: Int = StartOfStreamReader().detectStartOfStream(stream, 14)
 
         assertThat(indexOfStartOfStream).isEqualTo(7)
 
