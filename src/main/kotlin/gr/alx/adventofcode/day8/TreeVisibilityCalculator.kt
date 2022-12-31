fb09dc24ec46e9f72c04a9616afc077675d614b5
otinanism <otinanism@gmail.com>
2022-12-31 10:29:12 +0200

    Day 8 part 2.

--- src/main/kotlin/gr/alx/adventofcode/day8/TreeVisibilityCalculator.kt
+++ src/main/kotlin/gr/alx/adventofcode/day8/TreeVisibilityCalculator.kt
@@ -62,4 +62,72 @@ class TreeVisibilityCalculator {
     private fun isEdgeTree(lineIndex: Int, lineSize: Int, columnIndex: Int, columnSize: Int) =
         lineIndex == 0 || lineIndex == lineSize - 1 || columnIndex == 0 || columnIndex == columnSize - 1
 
+    fun calculateHighestScore(lines: List<String>): Int {
+        val grid: Array<IntArray> = parseGrid(lines)
+
+        var score = 0
+
+        grid
+            .forEachIndexed { lineIndex, columns ->
+                columns
+                    .forEachIndexed { columnIndex, tree ->
+                        val temp = calculateScore(grid, tree, lineIndex, columnIndex)
+                        if (temp > score) {
+                            score = temp
+                        }
+                    }
+            }
+        return score
+
+
+    }
+
+    private fun calculateScore(grid: Array<IntArray>, tree: Int, lineIndex: Int, columnIndex: Int): Int {
+        val line: IntArray = grid[lineIndex]
+        val column: List<Int> = grid.map { it[columnIndex] }
+
+        val lineSize = line.size
+        val columnSize = column.size
+
+        if (isEdgeTree(lineIndex, lineSize, columnIndex, columnSize)) {
+            return 0;
+        }
+
+        val lineScore = calculateLineScore(line, columnIndex, lineSize, tree)
+
+        val columnScore = calculateColumnScore(column, lineIndex, columnSize, tree)
+
+        return lineScore * columnScore
+    }
+
+
+    private fun calculateLineScore(line: IntArray, columnIndex: Int, lineSize: Int, tree: Int): Int {
+
+        val leftPart = line.toList().subList(0, columnIndex)
+        val rightPart = line.toList().subList(columnIndex + 1, lineSize)
+
+        val leftScore = leftPart.reversed().takeWhileInclusive { it < tree }.count()
+        val rightScore = rightPart.takeWhileInclusive { it < tree }.count()
+
+        return leftScore * rightScore
+    }
+
+    private fun calculateColumnScore(column: List<Int>, lineIndex: Int, columnSize: Int, tree: Int): Int {
+        val topPart = column.subList(0, lineIndex)
+        val bottomPart = column.subList(lineIndex + 1, columnSize)
+
+        val topScore = topPart.reversed().takeWhileInclusive { it < tree }.count()
+        val bottomScore = bottomPart.takeWhileInclusive { it < tree }.count()
+
+        return topScore * bottomScore
+    }
+
+    fun <T> List<T>.takeWhileInclusive(pred: (T) -> Boolean): List<T> {
+        var shouldContinue = true
+        return takeWhile {
+            val result = shouldContinue
+            shouldContinue = pred(it)
+            result
+        }
+    }
 }
--- src/test/kotlin/gr/alx/adventofcode/day8/TreeVisibilityCalculatorTest.kt
+++ src/test/kotlin/gr/alx/adventofcode/day8/TreeVisibilityCalculatorTest.kt
@@ -55,4 +55,41 @@ class TreeVisibilityCalculatorTest {
 
         assertThat(count).isEqualTo(1816)
     }
+
+    @Test
+    fun `should calculate highest scenic score `() {
+        val input = """
+            111
+            121
+            111
+        """.trimIndent()
+
+        val score: Int = TreeVisibilityCalculator().calculateHighestScore(input.lines())
+
+        assertThat(score).isEqualTo(1)
+    }
+
+    @Test
+    fun `should calculate highest scenic score for test input`() {
+        val input = """
+            30373
+            25512
+            65332
+            33549
+            35390
+        """.trimIndent()
+
+        val score: Int = TreeVisibilityCalculator().calculateHighestScore(input.lines())
+
+        assertThat(score).isEqualTo(8)
+    }
+
+    @Test
+    fun `should calculate highest scenic score for input`() {
+        val trees = ReadInputHelper.readLinesFromResource("/day8/trees.txt")
+
+        val score = TreeVisibilityCalculator().calculateHighestScore(trees)
+
+        assertThat(score).isEqualTo(383520)
+    }
 }
